76c3decd17d173f9d8cb6e295b161a56c9dd7ab5
esmolina <elenasmolinann@gmail.com>
2022-12-24 20:14:32 +0300

    fix: resolve merge conflicts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
 
 import { CartPage } from './components/cartPage/cartPage';
+import { Header } from './components/mainPage/header/Header';
+import { Main } from './components/mainPage/mainSection/Main';
+import { Footer } from './components/mainPage/footer/Footer';
+import {Card} from './components/productPage/productCard/Card';
+import { dataBase } from './dataBase/dataBase';
 
 const app = document.querySelector('.app') as HTMLDivElement;
 
@@ -17,3 +22,6 @@ app.append(cartPage);
 
 const footer = new Footer().element;
 app.append(footer);
+
+let test = new Card(dataBase[5])
+app.appendChild(test)
