c4d3b501a4e22d634e65652c6311266d76d6a665
esmolina <elenasmolinann@gmail.com>
2023-01-04 13:49:18 +0300

    feat: add view-button change

--- src/components/productPage/catalogMenu/catalogMenu.ts
+++ src/components/productPage/catalogMenu/catalogMenu.ts
@@ -4,27 +4,47 @@ import { ProductData } from '../../../dataBase/types';
 export class CatalogMenu {
   public catalogMenu: HTMLDivElement;
   public changeViewButton: HTMLButtonElement;
+  public isGridView: boolean;
+  // public newIconViewButton: HTMLSpanElement;
 
   constructor() {
+    this.isGridView = true;
     this.catalogMenu = this.createMenu();
     this.changeViewButton = this.createChangeViewButton();
     this.catalogMenu.append(this.changeViewButton);
   }
+
   handleChangeView(handler: () => void) {
     this.changeViewButton.addEventListener('click', () => {
       handler();
+      this.isGridView = !this.isGridView;
+      this.changeViewButton.replaceChildren(this.createNewIconViewButton());
     });
   }
+
   private createChangeViewButton() {
     const gridButton = document.createElement('button');
     gridButton.classList.add('catalog__menu-buttons');
     const iconGrid = document.createElement('span');
     iconGrid.classList.add('icon-list');
+
     gridButton.append(iconGrid);
     gridButton.type = 'button';
     return gridButton;
   }
-  private createMenu(): HTMLDivElement{
+
+  private createNewIconViewButton() {
+    const iconGrid = document.createElement('span');
+    if (this.isGridView) {
+      iconGrid.classList.add('icon-list');
+    }
+    if (!this.isGridView) {
+      iconGrid.classList.add('icon-grid');
+    }
+    return iconGrid;
+  }
+
+  private createMenu(): HTMLDivElement {
     const menuBlock = document.createElement('div');
     const filtersResetButton = document.createElement('button');
     const copyLinkButton = document.createElement('button');
@@ -32,7 +52,6 @@ export class CatalogMenu {
     const selectForm = new SelectSort().selectForme;
     const selectFormArrow = document.createElement('span');
     const emptyDiv = document.createElement('div');
-
 
     selectContainer.classList.add('catalog__menu-sort');
     selectForm.classList.add('catalog__menu-form');
@@ -53,8 +72,6 @@ export class CatalogMenu {
     copyLinkButton.append(iconCopyLink);
     copyLinkButton.append('Copy Link');
     copyLinkButton.type = 'button';
-
-
 
     emptyDiv.classList.add('catalog__menu-empty-div');
     emptyDiv.textContent = '';
